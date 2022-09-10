57a033f377a2ae0d8a8b86f9aef0b4efcd95cb85
Vedant Ghonge <vedantghonge0@gmail.com>
2022-09-11 00:05:01 +0530

    company logo scroll continuous

--- slider.js
+++ slider.js
@@ -1,7 +1,7 @@
 $(window).on('load', function(){
     initSmoothScrolling('.testimonial-components','smoothscroll');
-    console.log($('>div>div:first-of-type','.testimonial-components').outerWidth(true));
-    console.log($(".testimonial-components").width());
+    logoSmoothScrolling('.logo-components', 'logoScroll');
+    
   });
   
   function initSmoothScrolling(container,animation){
@@ -17,7 +17,7 @@ $(window).on('load', function(){
      
       var slidesVisible = $(container).width() / $('>div>div:first-of-type',container).outerWidth(true);	
       slidesVisible = Math.ceil(slidesVisible);
-      console.log(slidesVisible);
+      
   
    
       var slidesNumber = $('>div>div', container).length;
@@ -47,3 +47,47 @@ $(window).on('load', function(){
           $(this).addClass(cl);
       });
   }
+
+  function logoSmoothScrolling(container,animation){
+  
+    var sliderWidth = 0;	
+    var animationWidth = 0;	
+    
+
+    $('>div>div', container).each(function(){				
+        animationWidth += $(this).outerWidth(true);		
+    });
+    
+   
+    var slidesVisible = $(container).width() /$('>div>div:first-of-type',container).outerWidth(true);	
+    slidesVisible = Math.ceil(slidesVisible);
+    console.log(slidesVisible);
+
+ 
+    var slidesNumber = $('>div>div', container).length;
+    
+    var speed = 40;
+    
+    
+    
+
+    $('>div>div',container).slice(0,slidesVisible).clone().appendTo($('>div',container));	
+
+    
+    $('>div>div', container).each(function(){
+        sliderWidth += $(this).outerWidth(true);
+    });
+
+   
+    $('>div',container).css({'width':sliderWidth});
+   
+  
+// Insert styles to html
+  //   $("<style>@keyframes "+animation+" { 0% { margin-left: 0px; } 100% { margin-left: -"+animationWidth+"px; } } "+$('>div>div:first-of-type',container).selector+" { -webkit-animation: "+animation+" "+speed+"s linear infinite; -moz-animation: "+animation+" "+speed+"s linear infinite; -ms-animation: "+animation+" "+speed+"s linear infinite; -o-animation: "+animation+" "+speed+"s linear infinite; animation: "+animation+" "+speed+"s linear infinite; }</style>").appendTo("head");	
+    $("head").append("<style>@keyframes "+animation+" { 0% { margin-left: 0px; } 100% { margin-left: -"+animationWidth+"px; } } "+$('>div>div:first-of-type',container).selector+" { -webkit-animation: "+animation+" "+speed+"s linear infinite; -moz-animation: "+animation+" "+speed+"s linear infinite; -ms-animation: "+animation+" "+speed+"s linear infinite; -o-animation: "+animation+" "+speed+"s linear infinite; animation: "+animation+" "+speed+"s linear infinite; }</style>");
+    // restart the animation (e.g. for safari & ie)	
+    var cl = $(container).attr("class");
+    $(container).removeClass(cl).animate({'nothing':null}, 1, function () {
+        $(this).addClass(cl);
+    });
+}
